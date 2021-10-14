e8853373c189ff0c4bc96d5640cbaa56f51a700c
Flávio Tassan <ftassan@outlook.com>
2021-10-14 00:24:20 -0300

    Second exercise

---
+++ NumberOfCharacters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NumberOfCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the input string?");
+            var input = Console.ReadLine();
+            Console.WriteLine($"{input} has {input.Length} characters.");
+        }
+    }
+}
