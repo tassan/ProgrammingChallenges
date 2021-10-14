19a903ce0fe6fb0080c5cc63725f3891b78a3466
Flávio Tassan <ftassan@outlook.com>
2021-10-14 08:31:46 -0300

    Simple Math Exercise

---
+++ SimpleMath/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace SimpleMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the first number");
+            var inputFirstNumber = Console.ReadLine();
+            Console.WriteLine("What is the second number?");
+            var inputSecondNumber = Console.ReadLine();
+
+            int firstNumber = Convert.ToInt32(inputFirstNumber);
+            int secondNumber = Convert.ToInt32(inputSecondNumber);
+            Console.WriteLine($"{inputFirstNumber} + {inputSecondNumber} = {firstNumber + secondNumber}\n" +
+                              $"{inputFirstNumber} - {inputSecondNumber} = {firstNumber - secondNumber}\n" +
+                              $"{inputFirstNumber} * {inputSecondNumber} = {firstNumber * secondNumber}\n" +
+                              $"{inputFirstNumber} / {inputSecondNumber} = {firstNumber / secondNumber}\n");
+        }
+    }
+}
