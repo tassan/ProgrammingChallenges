8621f84cc39bdeaf91fe55172aafad865e70a7d5
Flávio Tassan <ftassan@outlook.com>
2021-10-14 00:28:26 -0300

    second challenge: user enters nothing, state that the user must enter something into the program

--- NumberOfCharacters/Program.cs
+++ NumberOfCharacters/Program.cs
@@ -8,6 +8,13 @@ namespace NumberOfCharacters
         {
             Console.WriteLine("What is the input string?");
             var input = Console.ReadLine();
+            while (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("You must inform the input string!");
+                Console.WriteLine("What is the input string?");
+                input = Console.ReadLine();
+            }
+                
             Console.WriteLine($"{input} has {input.Length} characters.");
         }
     }
