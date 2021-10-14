6c12deae068981293cb45f47b4f11530b716cebc
Flávio Tassan <ftassan@outlook.com>
2021-10-14 08:23:54 -0300

    Mad lib exercise

---
+++ MadLib/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MadLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a noun: ");
+            var noun = Console.ReadLine();
+            Console.WriteLine("Enter a verb: ");
+            var verb = Console.ReadLine();
+            Console.WriteLine("Enter a adjective: ");
+            var adjective = Console.ReadLine();
+            Console.WriteLine("Enter a adverb: ");
+            var adverb = Console.ReadLine();
+
+            Console.WriteLine($"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!");
+        }
+    }
+}
