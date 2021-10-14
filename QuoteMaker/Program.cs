b7a10426b53448fb8d16c722c8cbe57ee8db0095
Flávio Tassan <ftassan@outlook.com>
2021-10-14 08:12:27 -0300

    third exercise

---
+++ QuoteMaker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QuoteMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the quote?");
+            var quote = Console.ReadLine();
+            Console.WriteLine("Who said it?");
+            var author = Console.ReadLine();
+            
+            Console.WriteLine(author + " says, \"" + quote + "\"");
+        }
+    }
+}
