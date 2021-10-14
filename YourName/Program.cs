68a9753ccbd6c036c3798a62a6d87351271840ef
Flávio Tassan <flavio.filho@apinteractive.com.br>
2021-10-14 00:19:08 -0300

    First exercise

---
+++ YourName/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace YourName
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var output = $"Hello, {name}, nice to meet you!";
+            Console.WriteLine(output);
+        }
+    }
+}
